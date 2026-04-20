cd555eefbe9158667db51bc54c14d9c08fc7568d
Korbin VanDerSchaegen <vankorj@dunwoody.edu>
2026-04-20 16:06:10 -0500

    11.1

--- DBSys/Program.cs
+++ DBSys/Program.cs
@@ -1,24 +1,19 @@
-using DBSys.Data;
+﻿using DBSys.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-// Add DB connection
 builder.Services.AddDbContext<AppDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
-// Add services to the container.
 builder.Services.AddRazorPages();
 
+var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
